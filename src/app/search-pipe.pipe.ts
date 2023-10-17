1694040c872c68ec9253d60af73fff2d83759f1d
Jinal207 <chodavadiyajinal@gmail.com>
2023-10-17 17:27:11 +0530

    custom pipe final commit

--- src/app/search-pipe.pipe.ts
+++ src/app/search-pipe.pipe.ts
@@ -9,11 +9,21 @@ export class SearchPipePipe implements PipeTransform {
   transform(value: any[], query: string): any[] {
     if (!value.length) return value;
     if (!query) return value;
-
     query = query.toLowerCase();
 
+    // fatching data by key
+    // let res = value.filter((item: any) => {
+    //   return item.firstname.toLowerCase().includes(query) || item.lastname.toLowerCase().includes(query) || item.address.toLowerCase().includes(query)
+    // });
+
+    // fatching data by object iteration
     let res = value.filter((item: any) => {
-      return item.firstname.toLowerCase().includes(query) || item.lastname.toLowerCase().includes(query) || item.address.toLowerCase().includes(query)
+      for (const key in item) {
+        if (item[key].toString().toLowerCase().includes(query)) {
+          let res2 = item[key].toString().toLowerCase().includes(query)
+          return res2
+        }
+      }
     });
 
     return res;
