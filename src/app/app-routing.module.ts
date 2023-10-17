7bd59ab8cdd0098fab84a11cda5f0dd8d19d5f62
Jinal207 <chodavadiyajinal@gmail.com>
2023-10-17 12:28:48 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuiltPipeComponent } from './built-pipe/built-pipe.component';
+import { CustPipeComponent } from './cust-pipe/cust-pipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "builtinPipe", component: BuiltPipeComponent },
+  { path: "customPipe", component: CustPipeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
